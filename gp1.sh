4e47bc9d7b0cb7bbab6192dc92183517f7fe8e23
11awashington <44240990+11awashington@users.noreply.github.com>
2018-10-28 18:03:07 -0400

    Add files via upload

---
+++ gp1.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+# 1
+read -p "Who recently graduated from Farmingdale State College? " a
+while [[ ! $a = Sayem ]]; do 
+    read -p "Wrong, try again!" a
+done 
+echo "$a is correct!"
+# 2
+read -p "Who wants to pursue a career in cyber security? " a
+while [[ ! $a = Joseph ]]; do 
+    read -p "Wrong, try again!" a
+done 
+echo "$a is correct!"
+# 3
+read -p "Who is a member of Society of Hispanic Professional Engineers and National Society of Black Engineers " a
+while [[ ! $a = Eddy ]]; do 
+    read -p "Wrong, try again!" a
+done 
+echo "$a is correct!"
+# 4
+read -p "Who has four kids? " a
+while [[ ! $a = Princess ]]; do 
+    read -p "Wrong, try again!" a
+done 
+echo "$a is correct!"
+# 5 
+read -p "Who comes from a social work background? " a
+while [[ ! $a = Carolina ]]; do 
+    read -p "Wrong, try again!" a
+done  
+echo "$a is correct!"
+# 6 
+read -p "Who's an army vet? " a
+while [[ ! $a = Kin ]]; do 
+    read -p "Wrong, try again!" a
+done  
+echo "$a is correct!"
+# 7
+read -p "Who specializes in Android Development? " a
+while [[ ! $a = Jorge ]]; do 
+    read -p "Wrong, try again!" a
+done  
+echo "$a is correct!"
+# 8 
+read -p "Who's fluent in italian and spanish? " a
+while [[ ! $a = Francisco ]]; do 
+    read -p "Wrong, try again!" a
+done 
+echo "$a is correct!" 
+# 9
+read -p "Who's Dominican? " a
+while [[ ! $a = Reynaldo ]]; do 
+    read -p "Wrong, try again!" a
+done  
+echo "$a is correct!"
+# 10
+read -p "Who's a volunteer instructer at CodeNow? " a
+while [[ ! $a = Jonathan ]]; do 
+    read -p "Wrong, try again!" a
+done  
+echo "$a is correct!"
+# 11 
+read -p " Who was a CUNY Tech Prep Intern for NYC Tech Talent Pipeline in July 2017" a
+while [[ ! $a = Joseph ]]; do 
+    read -p "Wrong, try again!" a
+done  
+echo "$a is correct!"
+# 12 
+read -p "Who recieved their A+ IT certification at Per Scholas? " a
+while [[ ! $a = Marysol ]]; do 
+    read -p "Wrong, try again!" a
+done  
+echo "$a is correct!"
+# 13 
+read -p "Who's a Brooklyn College Graduate? " a
+while [[ ! $a = Osi ]]; do 
+    read -p "Wrong, try again!" a
+done  
+echo "$a is correct!"
